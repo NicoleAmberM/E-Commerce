5fc60f0df85c5ca191543802f76b2896e8e01091
Nicole Amber Mariscal <116238730+nicoleamber@users.noreply.github.com>
2023-06-30 17:43:26 +0800

    [BE] View and Update Cart (#5)

--- backend/app/serializers/cart_serializer.py
+++ backend/app/serializers/cart_serializer.py
@@ -1,6 +1,5 @@
 from app.models.cart import Cart
 from app.models.cart_item import CartItem
-from app.models.product import Product
 from app.serializers.datetime_serializer import DateTimeSerializer
 from rest_framework import serializers
 
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,5 @@
 from app.views.auth_view import LoginView, LogoutView, RegisterView
-from app.views.cart_view import AddToCartView
+from app.views.cart_view import AddToCartView, CartDetail
 from app.views.product_view import ProductDetail, ProductList
 from django.urls import path
 
@@ -7,7 +7,8 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("products/", ProductList.as_view()),
-    path("products/<slug:slug>", ProductDetail.as_view()),
-    path("cart/", AddToCartView.as_view()),
+    path("products/", ProductList.as_view(), name="product-list"),
+    path("products/<slug:slug>", ProductDetail.as_view(), name="product-detail"),
+    path("cart/", AddToCartView.as_view(), name="add-to-cart"),
+    path("cart/<int:customer_id>", CartDetail.as_view(), name="cart-detail"),
 ]
